a075c1aced5ac90539952dc428f337f2419d885d
Ankur Aggarwal <41417625+offiank@users.noreply.github.com>
2026-09-25 13:14:09 +0530

    Change string encoding to UTF-8 in TextParser

--- Java Code/req2task/src/main/java/com/ankur/req2task/ingest/TextParser.java
+++ Java Code/req2task/src/main/java/com/ankur/req2task/ingest/TextParser.java
@@ -2,11 +2,13 @@ package com.ankur.req2task.ingest;
 
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+
 @Component
 public class TextParser implements DocumentParser {
     @Override
     public String parse(byte[] content) {
-        return new String(content);
+        return new String(content, StandardCharsets.UTF_8);
     }
 
     @Override
